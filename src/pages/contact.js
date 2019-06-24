882685cb269285316d7cf1479db3cee02722b4dd
ehuang42 <ehuang42@gatech.edu>
2019-06-25 00:08:30 +0800

    initial

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import Header from "../components/header"
+
+export default () => (
+  <Layout>
+    <div style={{ color: `teal` }}>
+    <Header headerText="Hire me!"> </Header>
+      <p>Send Eva a message!</p>
+    </div>
+  </Layout>
+
+)
