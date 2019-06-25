75e72a27d95faa1bcc64d26eed3643de37f268eb
ehuang42 <ehuang42@gatech.edu>
2019-06-25 13:27:18 +0800

    change site name

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/Users/ehuang42/sourcecode/personalWeb/node_modules/gatsby-plugin-typography/gatsby-browser.js'),
+      options: {"plugins":[],"pathToConfigModule":"src/utils/typography"},
+    },{
+      plugin: require('/Users/ehuang42/sourcecode/personalWeb/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
