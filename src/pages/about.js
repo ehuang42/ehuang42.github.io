b4cedeeea406fe160b749c7885884a0f3a0404d8
ehuang42 <ehuang42@gatech.edu>
2019-06-28 15:43:52 +0800

    add new dropdown toggle

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,48 @@
 import React from "react"
 import Layout from "../components/layout"
+// import Button from 'react-bootstrap/Button'
+import Accordion from 'react-bootstrap/Accordion'
+import Card from 'react-bootstrap/Card'
 
-export default () => (
-  <Layout>
-  <div>
-    <h1>About me</h1>
-    <p>I’m good enough, I’m smart enough, and gosh darn it, people like me!</p>
-  </div>
-  </Layout>
-)
+
+class About extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            /**
+             * TODO
+             * Add state objects for the user inputs and anything else you may need to render the highchart.
+             */
+        };
+
+    }
+    render () {
+      return (
+          <Layout>
+            <p>nothing</p>
+            {/* <Button variant="primary" onClick={this.showAlert}>Primary</Button> */}
+           <div>
+             <Accordion defaultActiveKey="0">
+              <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="0">
+                  Click me!
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body>Hello! I'm the body</Card.Body>
+                </Accordion.Collapse>
+              </Card>
+              <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="1">
+                  Click me!
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="1">
+                  <Card.Body>Hello! I'm another body</Card.Body>
+                </Accordion.Collapse>
+              </Card>
+              </Accordion>
+            </div>
+          </Layout>
+    )}
+}
+
+export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,31 @@
 import React from "react"
 import Layout from "../components/layout"
 import photo from '../assets/goldman.jpg'
-
-export default () => (
-  <Layout>
-  <div style={{ margin: "3rem auto", maxWidth: 750 }}>
-  <p id="demo"></p>
-    <section id="setion03">
-      <h1>I'm a third year CS major</h1>
-      <p>
-        What do I like to do? Lots of course but definitely enjoy building
-        websites.
-      </p>
-      <img src={photo} className="avatar" alt=""></img>
-      <a href="#sec_2"><span></span>Scroll</a>
-    </section>
-
-    <section id="sec_2">
-    </section>
-  </div>
-  </Layout>
-)
+import 'bootstrap/dist/css/bootstrap.css';
 
 
+class App extends React.Component{
+  constructor(props) {
+      super(props);
+      this.state = {
+          /**
+           * TODO
+           * Add state objects for the user inputs and anything else you may need to render the highchart.
+           */
+          
+      };
+
+  }
+  render () {
+    return (
+        <Layout>
+          <h3>Welcome</h3><p>to my personal website</p>
+          <img src={photo} className="photo"></img>
+         
+        </Layout>
+  )}
+  
+  
+}
+
+export default App;
