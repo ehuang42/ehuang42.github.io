5d5cc6f797d21da762f8f123c0ab2b41ad708582
ehuang42 <ehuang42@gatech.edu>
2019-06-28 16:02:53 +0800

    fix link

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,11 +6,11 @@ import Form from 'react-bootstrap/Form'
 
 export default ({ children }) => (
   // nav bar on top
-  <div style={{ margin: `3rem auto`, maxWidth: 8000, padding: `0 1rem` }}>
-    <Navbar bg="light" variant="light">
-        <Navbar.Brand href="/about">Eva Huang</Navbar.Brand>
+  <div style={{ margin: `3rem auto`, maxWidth: 8000, padding: `0 2rem` }}>
+    <Navbar bg="light" variant="light" className="nav">
+        <Navbar.Brand href="/">Eva Huang</Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="">Home</Nav.Link>
+          <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="/about/">About</Nav.Link>
           <Nav.Link href="/contact">Contact</Nav.Link>
         </Nav>
