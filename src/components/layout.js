56c5cf0358936f6379fda328a5418b123410314c
ehuang42 <ehuang42@gatech.edu>
2019-06-28 15:57:09 +0800

    add nav bar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,24 @@
 import React from "react"
-import { Link } from "gatsby"
-
-const ListLink = props => (
-  <li style={{ display: "inline-block", marginRight: "1rem" }}>
-    <Link className="list_link" to={props.to}>{props.children}</Link>
-  </li>
-)
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
 
 export default ({ children }) => (
-  <div style={{ margin: "3rem auto", maxWidth: 830, padding: "0 1rem" }}>
-    <header style={{ marginBottom: "1.5rem" }}>
-      <Link className="coral" to="/" style={{}}>
-        <h3 style={{ display: "inline" }}>Eva Huang</h3>
-      </Link>
-      <ul className="nav-bar" style={{ listStyle: "none", float: "right" }}>
-        <ListLink to="/">Home</ListLink>
-        <ListLink to="/about/">About</ListLink>
-        <ListLink to="/contact/">Contact</ListLink>
-      </ul>
-    </header>    {children}
-  </div>
+  // nav bar on top
+  <div style={{ margin: `3rem auto`, maxWidth: 8000, padding: `0 1rem` }}>
+    <Navbar bg="light" variant="light">
+        <Navbar.Brand href="/about">Eva Huang</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="">Home</Nav.Link>
+          <Nav.Link href="/about/">About</Nav.Link>
+          <Nav.Link href="/contact">Contact</Nav.Link>
+        </Nav>
+        <Form inline>
+          <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-primary">Search</Button>
+        </Form>
+    </Navbar>
+    {children}
+    </div>
 )
